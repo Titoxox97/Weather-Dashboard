475f5f15e20732a032fa269f6c45add21deca904
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2021-12-21 22:19:19 -0500

    displaying data

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -24,53 +24,51 @@ button.addEventListener("click", (event) => {
     var coordinates = data[0];
     const { lat, lon } = coordinates;
     console.log(lat, lon);
-    var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${persApiKey}&units=metric`;
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${persApiKey}&units=imperial`;
     var weatherRes = await fetch(weatherUrl);
     var weatherData = await weatherRes.json();
     console.log(weatherData);
     //  Retrieve only the data needed using a list item method
     var symbolLink = `https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}.png`;
 
-    var weatherDetails = document.createElement("div");
-
-    var markup = `
-    <h2 class="currentCity" "${userInput}">  
-    <span class="currentCityDate">
-    (${moment
-      .unix(weatherData.current.dt)
-      .format("MM/DD/YYYY")})</span> <img id="weatherIcon" src="${symbolLink}"/>
-    </h2>
-    $("#currentWeather").append(
-      <p class="currentCityTemp">Temperature: ${
-        weatherData.current.temp + " &deg;F"
-      }</p>
-    );
-  
-    $("#currentWeather").append(
-      <p class="currentCityHumidity">Humidity: ${
-        weatherData.current.humidity + "%"
-      }</p>
-    );
-  
-    $("#currentWeather").append(
-      <p class="currentCityWindSpeed">Wind Speed: ${
-        weatherData.current.wind_speed + " MPH"
-      }</p>
-    );
-  
-    $("#currentWeather").append(
-      <p>
-      UV Index:
-      <span class="${weatherData.current.uvi}"
-        >${weatherData.current.uvi}</span
-      >
-    </p>
-    `;
-    weatherDetails.innerHTML = markup;
-    document.body.appendChild(weatherDetails);
     // make sure there's no duplicates and when called there will only be one request per city
     // var lsObjects = list.selectAll(".forecastCard");
     // var lsObjArray = Array.from(lsObjects);
+    weatherData.daily.forEach((weather) => {
+      var weatherDetails = document.createElement("div");
+
+      var markup = `
+    <h2 class="currentCity" "${userInput}">  
+    <span class="currentCityDate">
+    (${moment
+      .unix(weather.dt)
+      .format("MM/DD/YYYY")})</span> <img id="weatherIcon" src="${symbolLink}"/>
+    </h2>
+    
+      <p class="currentCityTemp">Temperature: ${
+        weather.temp.day + " &deg;F"
+      }</p>
+    
+  
+      <p class="currentCityHumidity">Humidity: ${weather.humidity + "%"}</p>
+    
+  
+      <p class="currentCityWindSpeed">Wind Speed: ${
+        weather.wind_speed + " MPH"
+      }</p>
+    
+  
+    
+      <p>
+      UV Index:
+      <span class="${weather.uvi}"
+        >${weather.uvi}</span
+      >
+    </p>
+    `;
+      weatherDetails.innerHTML = markup;
+      document.body.appendChild(weatherDetails);
+    });
   }
   startWeather();
 });
